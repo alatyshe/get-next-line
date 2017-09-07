3e6d6757b17e47e95048e14f360c97efcfc7fdb9
Andrey Latyshev <alatyshe@e3r4p5.unit.ua>
2017-09-07 20:10:03 +0300

    add gnl

---
+++ project/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alatyshe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/03 16:53:54 by alatyshe          #+#    #+#             */
+/*   Updated: 2017/01/11 20:33:14 by alatyshe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+# include <fcntl.h>
+
+int		get_next_line(int const fd, char **line);
+
+#endif
